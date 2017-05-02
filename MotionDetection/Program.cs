6d8bcf33c29f0af733ca16f5a8840ec024dbd1c5
Server_Fractal <valeras140@gmail.com>
2017-05-02 03:04:39 +0300

    1) Add save motions log to database 2) Delete TestDB project

--- MotionDetection/Program.cs
+++ MotionDetection/Program.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using IpCamLibrary;
+using IpCamLibrary.Database;
 
 namespace IpCamMotionDetection
 {
@@ -14,13 +15,27 @@ namespace IpCamMotionDetection
     {
         static void Main(string[] args)
         {
-            string config_XML = ConfigurationManager.AppSettings["PathToSettingsXML"];
-            string[] cams = SettingsManager.GetVLCConnectionStrings(config_XML);
+            try 
+            {
+               // InitCameras();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("При инициализации приложения произошла ошибка: " + ex.Message);
+                Console.ReadKey();
+                return;
+            }            
+
+            string [] sources;
+            using (JournalDbCobtext db = new JournalDbCobtext())
+            {
+                sources = db.Cameras.Where(x => (x.Title == "1-414" || x.Title == "1-321")).Select(x => x.Source).ToArray();
+            }
 
             DetectionController dc = new DetectionController();
             dc.DataRecived += OnDataRecived;
-            dc.CycleInterval = 1;
-            dc.StartCams(cams[0], cams[6]);
+            dc.CycleInterval = 60;
+            dc.StartCams(sources);
 
             while (true)
             {               
@@ -36,20 +51,65 @@ namespace IpCamMotionDetection
             e.PrintConsole();
             lock (locker)
             {
-                SaveToFile(e);
+                SaveToDatabase(e);
             }
         }
 
         static object locker = new object();
-        public static void SaveToFile(DetectingEventArgs e)
+        public static void SaveToDatabase(DetectingEventArgs e)
         {
-            using (StreamWriter sr = new StreamWriter("C:\\cam_log.txt", true))
+            using (JournalDbCobtext db = new JournalDbCobtext())
             {
-                sr.WriteLine("Source " + e.DataSource +
-                                 " Motions " + e.AverageMotions +
-                                 " Count " + e.TotalCount +
-                                 " Time " + e.TimeStart.Hour + ":" + e.TimeStart.Minute + ":" + e.TimeStart.Second);
-            };
-        }   
+                db.Logs.Add(new Log()
+                {
+                    AverageMotions = e.AverageMotions,
+                    TimeStart = e.TimeStart,
+                    TimeFinish = e.TimeFinish,
+                    TotalCount = e.TotalCount,
+                    Camera = db.Cameras.Where(x => x.Source == e.DataSource).SingleOrDefault()      
+                });
+
+                db.SaveChanges();
+                Console.WriteLine("Saved to DB");
+            }
+        }
+
+        public static void InitCameras()
+        {
+            SettingsManager sm;
+            try
+            {
+                string path = ConfigurationManager.AppSettings["PathToSettingsXML"];
+                sm = new SettingsManager(path);
+                sm.LoadConfig();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            using (JournalDbCobtext db = new JournalDbCobtext())
+            {
+                //  *** Нужно не удалять, а обновлять в случае необходимости ***
+
+                //db.Cameras.RemoveRange(db.Cameras); 
+                //db.SaveChanges();
+
+                foreach (var set in sm.SettingsList)
+                {
+                    db.Cameras.Add(new Camera()
+                    {
+                        Title = set.Title,
+                        Source = "http://" + sm.Ip_vlc + ":" + set.Port_vlc
+                    });
+                }
+                db.SaveChanges();
+
+                Console.WriteLine("Cameras updated");
+            }
+        }
+
+
+
     }    
 }
