2f6f7e78a8a3bb2e9c1725a569f650cb34c94ed6
Server_Fractal <valeras140@gmail.com>
2017-05-12 12:21:09 +0300

    Return DataProvider to project

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using IpCamLibrary.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = DataProvider.GetData(DateTime.Now, 8);
+
+        }
+    }
+}
